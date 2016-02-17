917f1f5fc2013f7cc237b26ecb5ad0b448eae6fa
matsavar <cihanblt@gmail.com>
2016-02-17 19:48:58 +0200

    created SherlockAndSquares.java file

---
+++ src/main/java/com/cb/Implementation/SherlockAndSquares.java
@@ -0,0 +1,17 @@
+package com.cb.Implementation;
+
+import java.util.Scanner;
+
+/**
+ * Created by c.bulut on 2/17/2016.
+ */
+public class SherlockAndSquares {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        for(int a0 = 0 ; a0 < t; a0++){
+
+        }
+    }
+}
